33286e5411a1ec1f0905518bfc473c46490f3d9e
chrissblife <w0657251@selu.edu>
2018-04-14 23:23:33 -0500

    changes

---
+++ public/javascripts/emrdb.js
@@ -0,0 +1,33 @@
+let emr;
+emr = [
+    {
+        "id": "0000-00001",
+        "name": "Monica Latte",
+        "address": "444 Coffee Ave",
+        "medications": [
+            "PRINIVIL TABS 20 MG (LISINOPRIL) 1 po qd",
+            "HUMULIN INJ 70/30 (INSULIN REG & ISOPHANE (HUMAN)) 20 units ac breakfast"
+        ],
+        "birthdate": "04/04/1950",
+        "provider": "Dr. Carl Savem"
+    },
+    {
+        "id": "0000-00002",
+        "name": "Steve New",
+        "address": "123 Fifth St",
+        "medications": [
+            "PRINIVIL TABS 20 MG (LISINOPRIL) 1 po qd",
+        ],
+        "birthdate": "04/04/1950",
+        "provider": "Dr. Carl Savem"
+    },
+    {
+        "id": "0000-44444",
+        "name": "Monica Latte",
+        "address": "444 Coffee Ave",
+        "medications": [
+            "Ibuprofen 800 mg",
+        ],
+        "birthdate": "04/04/1950",
+    }
+]
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'EMR Home Page' });
 });
-
+/* GET all EMRs. */
+router.get('/emr', function(req, res, next) {
+    res.render('emr', { title: 'emr' });
+});
 module.exports = router;
+**under views add a .jade file named emr.jade and paste
+extends layout
+
+block content
+p All EMRs:
+    script(type='text/javascript' src='../public/javascripts/emrdb.js').
+x = emr
+console.log(x)
